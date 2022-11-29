9ec1fdcf11c3aee2807cec8e9485ea81bbdfb955
tmhMinhHieu <115906065+tmhMinhHieu@users.noreply.github.com>
2022-11-29 20:51:56 +0700

    Update

--- src/main/java/com/qlbh/SignController.java
+++ src/main/java/com/qlbh/SignController.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,40 +39,32 @@ public class SignController {
         private Button signIn;
         @FXML
         private Label signUp;
-<<<<<<< HEAD
-        @FXML
-        private Button cancel;
-        @FXML
-        private AnchorPane scenePane;
 
-
-    public void SignIn(ActionEvent event) throws IOException {
-        String userName = user.getText().toString();
-        String passW = password.getText().toString();
-
-        // sign admin
-        if(userName.equals(userName) && pass.equals(passW)){
-            if(Manager){
-                fxmlViewName = "menu-admin-view.fxml";
-            }
-            else {
-                fxmlViewName = "employee-view.fxml";
-            }
-            Parent root = FXMLLoader.load(getClass().getResource(fxmlViewName));
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setTitle("Quản lý bán hàng");
-            stage.setScene(scene);
-            stage.show();
-        }
-        else
-            System.out.println("sign in fail");
-    }
-
-    public void showPass(ActionEvent event)
-    {
-        if(showPassword.isSelected())
-        {
+//    public void SignIn(ActionEvent event) throws IOException {
+//        String userName = user.getText().toString();
+//        String passW = password.getText().toString();
+//
+//        // sign admin
+//        if(userName.equals(userName) && pass.equals(passW)){
+//            if(Manager){
+//                fxmlViewName = "menu-admin-view.fxml";
+//            }
+//            else {
+//                fxmlViewName = "employee-view.fxml";
+//            }
+//            Parent root = FXMLLoader.load(getClass().getResource(fxmlViewName));
+//            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//            Scene scene = new Scene(root);
+//            stage.setTitle("Quản lý bán hàng");
+//            stage.setScene(scene);
+//            stage.show();
+//        }
+//        else
+//
+//      System.out.println("sign in fail");
+//    }
+    public void showPass(ActionEvent event) {
+        if (showPassword.isSelected()) {
             passwordText.setText(password.getText());
             passwordText.setVisible(true);
             password.setVisible(false);
@@ -82,7 +73,7 @@ public class SignController {
         password.setText(passwordText.getText());
         password.setVisible(true);
         passwordText.setVisible(false);
-=======
+    }
 
     public void AlertShow(String content, Alert.AlertType type){
         Alert alert = new Alert(type);
@@ -132,13 +123,6 @@ public class SignController {
         }
     }
 
-    public void showPass(ActionEvent e) {
-//        System.out.println("showpass");
-//        if(showPassword.isSelected()) {
-//            password.accessibleTextProperty();
-//        }
->>>>>>> 85488c4bf2f68d4be5f23ef26c59e9cee8aaf6cc
-    }
 
     public void cancel(ActionEvent e) throws IOException {
         user.setText("");
