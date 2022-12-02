271a698e233256c65b9e44b709860f0a62e1a91c
Tien Nguyen <sinhtien0125@gmail.com>
2022-12-02 08:43:30 +0700

    up validator add emp

--- src/main/java/com/qlbh/menuAdminController.java
+++ src/main/java/com/qlbh/menuAdminController.java
@@ -1,5 +1,6 @@
 package com.qlbh;
 
+import com.config.JDBC;
 import com.services.EmployessServices;
 import javafx.scene.control.Alert;
 import project.Employess;
@@ -9,8 +10,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 
 public class menuAdminController extends menuView {
@@ -35,20 +35,54 @@ public class menuAdminController extends menuView {
        this.OutputBill(e);
    }
 
+   private boolean validator(String val){
+       if(val.isEmpty()){
+           ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
+           return false;
+       }
+       return true;
+   }
+
+   private boolean validator(LocalDate date){
+       if(date == null){
+           return false;
+       }
+       return true;
+   }
+
+   public boolean isUserExist(String userName) throws SQLException {
+
+       Connection connection = JDBC.getCnn();
+       PreparedStatement stm = connection.prepareStatement("select user from employees");
+       ResultSet rs = stm.executeQuery();
+       if(rs.next()){
+           return true;
+       }
+       return false;
+   }
    public void addEmployee() throws SQLException {
-       String lastName= txt_last_name.getText();
-       String firstName = txt_first_name.getText();
-       LocalDate date = txt_date.getValue();
-       String adress = txt_adress.getText();
        String userName= txt_user_name.getText();
-       String passWord = txt_password.getText();
-       String phone = txt_phone.getText();
-       String email = txt_email.getText();
+       if(!isUserExist(userName)){
+           String lastName= txt_last_name.getText();
+           String firstName = txt_first_name.getText();
+           LocalDate date = txt_date.getValue();
+           String adress = txt_adress.getText();
+           String passWord = txt_password.getText();
+           String phone = txt_phone.getText();
+           String email = txt_email.getText();
 
-       Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,userName,passWord,false);
-       EmployessServices.addEmployees(employess);
-
-//       ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
+           if(validator(lastName) && validator(firstName) && validator(adress) && validator(userName)
+                   && validator(passWord) && validator(email) && validator(date)){
+               Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,userName,passWord,false);
+               EmployessServices.addEmployees(employess);
+               ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
+           }else{
+               ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
+           }
+       }
+       else{
+           ShowAlert.show("tài khoản đã tồn tại", Alert.AlertType.INFORMATION);
+       }
    }
 
    public void goBack(ActionEvent e) throws IOException {
