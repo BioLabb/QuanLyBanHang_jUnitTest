41ce8c8a5d93e536c49878d897b22d9d4597369d
Tien Nguyen <sinhtien0125@gmail.com>
2022-12-09 21:02:09 +0700

    fix err

--- src/main/java/com/qlbh/menuAdminController.java
+++ src/main/java/com/qlbh/menuAdminController.java
@@ -413,35 +413,35 @@ public class menuAdminController implements Initializable {
     }
 
     public void searchProductById(ActionEvent event) {
-//        if (validator(product_id.getText())) {
-//            int id = Integer.parseInt(product_id.getText());
-//            List<TableProduct> tableProducts = productObservableList.stream().filter(pt -> pt.getProductID() == id).collect(Collectors.toList());
-//
-//            ObservableList<TableProduct> tmp = FXCollections.observableArrayList();
-//            for (TableProduct tableProduct : tableProducts) {
-//                tmp.add(tableProduct);
-//            }
-//            table_product.setItems(tmp);
-//        } else {
-//            ShowAlert.show("Mã đơn hàng trống", Alert.AlertType.WARNING);
-//            table_product.setItems(productObservableList);
-//        }
+        if (validator(product_id.getText())) {
+            int id = Integer.parseInt(product_id.getText());
+            List<TableProduct> tableProducts = productObservableList.stream().filter(pt -> pt.getProductID() == id).collect(Collectors.toList());
+
+            ObservableList<TableProduct> tmp = FXCollections.observableArrayList();
+            for (TableProduct tableProduct : tableProducts) {
+                tmp.add(tableProduct);
+            }
+            table_product.setItems(tmp);
+        } else {
+            ShowAlert.show("Mã đơn hàng trống", Alert.AlertType.WARNING);
+            table_product.setItems(productObservableList);
+        }
     }
 
     public void searchProductByProductName(ActionEvent event) {
-//        if (validator(product_Name.getText().trim())) {
-//            String name = product_Name.getText().trim();
-//            List<TableProduct> tableProducts = productObservableList.stream().filter(pt -> pt.getProductName().equals(name)).collect(Collectors.toList());
-//
-//            ObservableList<TableProduct> tmp = FXCollections.observableArrayList();
-//            for (TableProduct tableProduct : tableProducts) {
-//                tmp.add(tableProduct);
-//            }
-//            table_product.setItems(tmp);
-//        } else {
-//            ShowAlert.show("Mã đơn hàng trống", Alert.AlertType.WARNING);
-//            table_product.setItems(productObservableList);
-//        }
+        if (validator(product_Name.getText().trim())) {
+            String name = product_Name.getText().trim();
+            List<TableProduct> tableProducts = productObservableList.stream().filter(pt -> pt.getProductName().equals(name)).collect(Collectors.toList());
+
+            ObservableList<TableProduct> tmp = FXCollections.observableArrayList();
+            for (TableProduct tableProduct : tableProducts) {
+                tmp.add(tableProduct);
+            }
+            table_product.setItems(tmp);
+        } else {
+            ShowAlert.show("Mã đơn hàng trống", Alert.AlertType.WARNING);
+            table_product.setItems(productObservableList);
+        }
     }
 //}
 //<<<<<<< HEAD
@@ -454,216 +454,216 @@ public class menuAdminController implements Initializable {
 //
 //
 //// ------------ THÊM NHÂN VIÊN-------------
-//   // kiểm tra rỗng
-//   private boolean validator(String val){
-//       if(val.isEmpty()){
-//           return false;
-//       }
-//       return true;
-//   }
-//
-//   // kiểm tra ngày rỗng
-//   private boolean validator(LocalDate date){
-//       if(date == null){
-//           return false;
-//       }
-//       return true;
-//   }
-//
-//   private boolean isUserNameEmployee(String user){
-//       if(user.startsWith("NV"))
-//           return true;
-//       return false;
-//   }
-//
-//   private boolean isCheckLength(String string,int lengthMin,int lengthMax){
-//       if(string.length() >= lengthMin && string.length() <= lengthMax)
-//           return true;
-//       return false;
-//   }
-//
-//   // kiểm tra tài khoản tồn tại hay chưa
-//   public boolean isUserExist(String userName) throws SQLException {
-//
-//       Connection connection = JDBC.getCnn();
-//       PreparedStatement stm = connection.prepareStatement("select user from employees where user = ?");
-//       stm.setString(1, userName);
-//       ResultSet rs = stm.executeQuery();
-//       if(rs.next()){
-//           return true;
-//       }
-//       return false;
-//   }
-//
-//   public boolean isPhone(String s){
-//       Pattern p = Pattern.compile("^[0-9]{10}$");
-//       if(p.matcher(s).find())
-//           return true;
-//       return false;
-//   }
-//
-//   public boolean isMail(String s){
-//        Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z]+(\\.com)$");
-//        if(p.matcher(s).find())
-//            return true;
-//        return false;
-//   }
-//
-//
-//
-//   public void addEmployee() throws SQLException {
-//       String userName= txt_user_name.getText();
-//       // kiểm tra tài khoản đã tạo hay chưa
-//       if(!isUserExist("NV"+userName)){
-//           String lastName= txt_last_name.getText();
-//           String firstName = txt_first_name.getText();
-//           LocalDate date = txt_date.getValue();
-//           String adress = txt_adress.getText();
-//           String passWord = txt_password.getText();
-//           String phone = txt_phone.getText();
-//           String email = txt_email.getText();
-//
-//           // kiểm tra thông tin dã được nhập đầy đủ chưa
-//           if(validator(lastName) && validator(firstName) && validator(adress) && validator(userName)
-//                   && validator(passWord) && validator(email)){
-//               if(!(isCheckLength(userName,6,16))){
-//                   ShowAlert.show("user từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
-//               }
-//               else if(!isCheckLength(passWord,8,16)){
-//                   ShowAlert.show("password từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
-//               }
-//               else if(userName.equals(EmployeesStore.getEmployess().getUser())){
-//                   ShowAlert.show("password không được trùng với username", Alert.AlertType.WARNING);
-//               }else if(!validator(date)){
-//                   ShowAlert.show("Ngày tháng không hợp lê", Alert.AlertType.WARNING);
-//               }else if(!isPhone(phone)){
-//                   ShowAlert.show("Số điện thoại không hợp lệ", Alert.AlertType.WARNING);
-//               }else if(!isMail(email)){
-//               ShowAlert.show("Email không hợp lệ", Alert.AlertType.WARNING);
-//           }
-//           else {
-//                   String nv = lb_nv.getText();
-//                   String userSignUp = nv + txt_user_name.getText();
-//                   Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,
-//                           userSignUp,passWord,false);
-//                   EmployessServices.addEmployees(employess);
-//                   ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
-//               }
-//           }else{
-//               ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
-//           }
-//       }
-//       else{
-//           ShowAlert.show("tài khoản đã tồn tại", Alert.AlertType.INFORMATION);
-//       }
-//   }
-//
-//   public void goBack(ActionEvent e) throws IOException {
-//       menuView.nextPage(e,"sign-view.fxml","Sign");
-//   }
-//
-//   public void removeEmployee(ActionEvent e) throws IOException {
-//
-//       menuView.nextPage(e,"remove-employee-view.fxml","xóa nhân viên");
-//   }
+   // kiểm tra rỗng
+   private boolean validator(String val){
+       if(val.isEmpty()){
+           return false;
+       }
+       return true;
+   }
+
+   // kiểm tra ngày rỗng
+   private boolean validator(LocalDate date){
+       if(date == null){
+           return false;
+       }
+       return true;
+   }
+
+   private boolean isUserNameEmployee(String user){
+       if(user.startsWith("NV"))
+           return true;
+       return false;
+   }
+
+   private boolean isCheckLength(String string,int lengthMin,int lengthMax){
+       if(string.length() >= lengthMin && string.length() <= lengthMax)
+           return true;
+       return false;
+   }
+
+   // kiểm tra tài khoản tồn tại hay chưa
+   public boolean isUserExist(String userName) throws SQLException {
+
+       Connection connection = JDBC.getCnn();
+       PreparedStatement stm = connection.prepareStatement("select user from employees where user = ?");
+       stm.setString(1, userName);
+       ResultSet rs = stm.executeQuery();
+       if(rs.next()){
+           return true;
+       }
+       return false;
+   }
+
+   public boolean isPhone(String s){
+       Pattern p = Pattern.compile("^[0-9]{10}$");
+       if(p.matcher(s).find())
+           return true;
+       return false;
+   }
+
+   public boolean isMail(String s){
+        Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z]+(\\.com)$");
+        if(p.matcher(s).find())
+            return true;
+        return false;
+   }
+
+
+
+   public void addEmployee() throws SQLException {
+       String userName= txt_user_name.getText();
+       // kiểm tra tài khoản đã tạo hay chưa
+       if(!isUserExist("NV"+userName)){
+           String lastName= txt_last_name.getText();
+           String firstName = txt_first_name.getText();
+           LocalDate date = txt_date.getValue();
+           String adress = txt_adress.getText();
+           String passWord = txt_password.getText();
+           String phone = txt_phone.getText();
+           String email = txt_email.getText();
+
+           // kiểm tra thông tin dã được nhập đầy đủ chưa
+           if(validator(lastName) && validator(firstName) && validator(adress) && validator(userName)
+                   && validator(passWord) && validator(email)){
+               if(!(isCheckLength(userName,6,16))){
+                   ShowAlert.show("user từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
+               }
+               else if(!isCheckLength(passWord,8,16)){
+                   ShowAlert.show("password từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
+               }
+               else if(userName.equals(EmployeesStore.getEmployess().getUser())){
+                   ShowAlert.show("password không được trùng với username", Alert.AlertType.WARNING);
+               }else if(!validator(date)){
+                   ShowAlert.show("Ngày tháng không hợp lê", Alert.AlertType.WARNING);
+               }else if(!isPhone(phone)){
+                   ShowAlert.show("Số điện thoại không hợp lệ", Alert.AlertType.WARNING);
+               }else if(!isMail(email)){
+               ShowAlert.show("Email không hợp lệ", Alert.AlertType.WARNING);
+           }
+           else {
+                   String nv = lb_nv.getText();
+                   String userSignUp = nv + txt_user_name.getText();
+                   Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,
+                           userSignUp,passWord,false);
+                   EmployessServices.addEmployees(employess);
+                   ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
+               }
+           }else{
+               ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
+           }
+       }
+       else{
+           ShowAlert.show("tài khoản đã tồn tại", Alert.AlertType.INFORMATION);
+       }
+   }
+
+   public void goBack(ActionEvent e) throws IOException {
+       menuView.nextPage(e,"sign-view.fxml","Sign");
+   }
+
+   public void removeEmployee(ActionEvent e) throws IOException {
+
+       menuView.nextPage(e,"remove-employee-view.fxml","xóa nhân viên");
+   }
 //=======
-//
-//
-//
+
+
+
 //    // ------------ THÊM NHÂN VIÊN-------------
     // kiểm tra rỗng
-    private boolean validator(String val){
-        if(val.isEmpty()){
-            return false;
-        }
-        return true;
-    }
-
-    // kiểm tra ngày rỗng
-    private boolean validator(LocalDate date){
-        if(date == null){
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isUserNameEmployee(String user){
-        if(user.startsWith("NV"))
-            return true;
-        return false;
-    }
-
-    private boolean isCheckLength(String string,int lengthMin,int lengthMax){
-        if(string.length() >= lengthMin && string.length() <= lengthMax)
-            return true;
-        return false;
-    }
-
-    // kiểm tra tài khoản tồn tại hay chưa
-    public boolean isUserExist(String userName) throws SQLException {
-
-        Connection connection = JDBC.getCnn();
-        PreparedStatement stm = connection.prepareStatement("select user from employees where user = ?");
-        stm.setString(1, userName);
-        ResultSet rs = stm.executeQuery();
-        if(rs.next()){
-            return true;
-        }
-        return false;
-    }
-
-
-    public void addEmployee() throws SQLException {
-        String userName= txt_user_name.getText();
-        // kiểm tra tài khoản đã tạo hay chưa
-        if(!isUserExist("NV"+userName)){
-            String lastName= txt_last_name.getText();
-            String firstName = txt_first_name.getText();
-            LocalDate date = txt_date.getValue();
-            String adress = txt_adress.getText();
-            String passWord = txt_password.getText();
-            String phone = txt_phone.getText();
-            String email = txt_email.getText();
-
-            // kiểm tra thông tin dã được nhập đầy đủ chưa
-            if(validator(lastName) && validator(firstName) && validator(adress) && validator(userName)
-                    && validator(passWord) && validator(email)){
-                if(!(isCheckLength(userName,6,16))){
-                    ShowAlert.show("user từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
-                }
-                else if(!isCheckLength(passWord,6,16)){
-                    ShowAlert.show("password từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
-                }
-                else if(userName.equals(EmployeesStore.getEmployess().getUser())){
-                    ShowAlert.show("password không được trùng với username", Alert.AlertType.WARNING);
-                }else if(!validator(date)){
-                    ShowAlert.show("Ngày tháng không hợp lê", Alert.AlertType.WARNING);
-                }
-                else {
-                    String nv = lb_nv.getText();
-                    String userSignUp = nv + txt_user_name.getText();
-                    Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,
-                            userSignUp,passWord,false);
-                    EmployessServices.addEmployees(employess);
-                    ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
-                }
-            }else{
-                ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
-            }
-        }
-        else{
-            ShowAlert.show("tài khoản đã tồn tại", Alert.AlertType.INFORMATION);
-        }
-    }
-
-    public void goBack(ActionEvent e) throws IOException {
-        menuView.nextPage(e,"sign-view.fxml","Sign");
-    }
-
-    public void removeEmployee(ActionEvent e) throws IOException {
-
-        menuView.nextPage(e,"remove-employee-view.fxml","xóa nhân viên");
-    }
+//    private boolean validator(String val){
+//        if(val.isEmpty()){
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    // kiểm tra ngày rỗng
+//    private boolean validator(LocalDate date){
+//        if(date == null){
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    private boolean isUserNameEmployee(String user){
+//        if(user.startsWith("NV"))
+//            return true;
+//        return false;
+//    }
+//
+//    private boolean isCheckLength(String string,int lengthMin,int lengthMax){
+//        if(string.length() >= lengthMin && string.length() <= lengthMax)
+//            return true;
+//        return false;
+//    }
+//
+//    // kiểm tra tài khoản tồn tại hay chưa
+//    public boolean isUserExist(String userName) throws SQLException {
+//
+//        Connection connection = JDBC.getCnn();
+//        PreparedStatement stm = connection.prepareStatement("select user from employees where user = ?");
+//        stm.setString(1, userName);
+//        ResultSet rs = stm.executeQuery();
+//        if(rs.next()){
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//
+//    public void addEmployee() throws SQLException {
+//        String userName= txt_user_name.getText();
+//        // kiểm tra tài khoản đã tạo hay chưa
+//        if(!isUserExist("NV"+userName)){
+//            String lastName= txt_last_name.getText();
+//            String firstName = txt_first_name.getText();
+//            LocalDate date = txt_date.getValue();
+//            String adress = txt_adress.getText();
+//            String passWord = txt_password.getText();
+//            String phone = txt_phone.getText();
+//            String email = txt_email.getText();
+//
+//            // kiểm tra thông tin dã được nhập đầy đủ chưa
+//            if(validator(lastName) && validator(firstName) && validator(adress) && validator(userName)
+//                    && validator(passWord) && validator(email)){
+//                if(!(isCheckLength(userName,6,16))){
+//                    ShowAlert.show("user từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
+//                }
+//                else if(!isCheckLength(passWord,6,16)){
+//                    ShowAlert.show("password từ 8 đến 16 ký tự", Alert.AlertType.INFORMATION);
+//                }
+//                else if(userName.equals(EmployeesStore.getEmployess().getUser())){
+//                    ShowAlert.show("password không được trùng với username", Alert.AlertType.WARNING);
+//                }else if(!validator(date)){
+//                    ShowAlert.show("Ngày tháng không hợp lê", Alert.AlertType.WARNING);
+//                }
+//                else {
+//                    String nv = lb_nv.getText();
+//                    String userSignUp = nv + txt_user_name.getText();
+//                    Employess employess = new Employess(lastName,firstName, Date.valueOf(date),email,phone,adress,
+//                            userSignUp,passWord,false);
+//                    EmployessServices.addEmployees(employess);
+//                    ShowAlert.show("Thêm thành công", Alert.AlertType.INFORMATION);
+//                }
+//            }else{
+//                ShowAlert.show("Chưa điền đầy đủ thông tin", Alert.AlertType.WARNING);
+//            }
+//        }
+//        else{
+//            ShowAlert.show("tài khoản đã tồn tại", Alert.AlertType.INFORMATION);
+//        }
+//    }
+//
+//    public void goBack(ActionEvent e) throws IOException {
+//        menuView.nextPage(e,"sign-view.fxml","Sign");
+//    }
+//
+//    public void removeEmployee(ActionEvent e) throws IOException {
+//
+//        menuView.nextPage(e,"remove-employee-view.fxml","xóa nhân viên");
+//    }
 
 //    //Day la du lieu rieng cua Hieu neu cai kia load khong duoc cu doi table view roi xai khong anh huong den
 //    //database trong sql
